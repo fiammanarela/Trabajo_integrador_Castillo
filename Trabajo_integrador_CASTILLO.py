5826dfa0fa9d2fb0689b287ae58fb0b4cfad0cc2
fiammanarela <fncastillorossi@itel.edu.ar>
2025-11-11 21:43:24 -0300

    cesar_cifrado

--- Trabajo_integrador_CASTILLO.py
+++ Trabajo_integrador_CASTILLO.py
@@ -6,3 +6,23 @@ import random
 def xor_cifrado(texto, key):
     return ''.join(chr(ord(c) ^ ord(key[i % len(key)])) for i, c in enumerate(texto))
 
+# CIFRADO CESAR
+def cesar_cifrado(texto, key, decrypt=False):
+    try:
+        shift = int(key) % 26
+    except:
+        return "La clave debe ser un número"
+    if decrypt:
+        shift = -shift
+    result = ""
+    for char in texto:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            result += char
+    return result
+
+
+
+
