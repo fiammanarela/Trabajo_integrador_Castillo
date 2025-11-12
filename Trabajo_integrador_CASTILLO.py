8df673b9f19f12ed811d4a8c4b10e5478678135d
fiammanarela <fncastillorossi@itel.edu.ar>
2025-11-11 21:48:56 -0300

    playfair_cifrado

--- Trabajo_integrador_CASTILLO.py
+++ Trabajo_integrador_CASTILLO.py
@@ -43,5 +43,58 @@ def vigenere_cifrado(texto, key, decrypt=False):
     return resultado
 
 
+# PLAYFAIR
+def matriz(key):
+    key = key.upper().replace('J', 'I')
+    matrix = []
+    used = set()
+    for char in key + 'ABCDEFGHIKLMNOPQRSTUVWXYZ':
+        if char not in used and char.isalpha():
+            matrix.append(char)
+            used.add(char)
+    return [matrix[i:i+5] for i in range(0, 25, 5)]
+
+
+def posicion(matrix, char):
+    for r in range(5):
+        for c in range(5):
+            if matrix[r][c] == char:
+                return r, c
+    return None, None
+
+
+def playfair_cifrado(text, key, decrypt=False):
+    matrix = matriz(key)
+    text = text.upper().replace('J', 'I')
+    pairs = []
+    i = 0
+    while i < len(text):
+        a = text[i]
+        b = 'X'
+        if i + 1 < len(text) and text[i + 1] != a:
+            b = text[i + 1]
+            i += 1
+        pairs.append(a + b)
+        i += 1
+
+    result = ""
+    for pair in pairs:
+        r1, c1 = posicion(matrix, pair[0])
+        r2, c2 = posicion(matrix, pair[1])
+        if r1 == r2:
+            if decrypt:
+                result += matrix[r1][(c1 - 1) % 5] + matrix[r2][(c2 - 1) % 5]
+            else:
+                result += matrix[r1][(c1 + 1) % 5] + matrix[r2][(c2 + 1) % 5]
+        elif c1 == c2:
+            if decrypt:
+                result += matrix[(r1 - 1) % 5][c1] + matrix[(r2 - 1) % 5][c2]
+            else:
+                result += matrix[(r1 + 1) % 5][c1] + matrix[(r2 + 1) % 5][c2]
+        else:
+            result += matrix[r1][c2] + matrix[r2][c1]
+
+    return result
+
 
 
